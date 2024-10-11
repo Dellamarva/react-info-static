b7e5a6cee21ba9e272c95ab3684530019d836459
Greg Dellamura <g.dellamura@Gregs-MacBook-Pro-2.local>
2024-10-11 11:39:04 -0400

    first commit

---
+++ src/components/Navbar.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function Navbar() {
+    return (
+        <nav className="navbar">
+            <img className="react-logo" src="logo192.png" />
+            <h3 className="react-facts">ReactFacts</h3>
+            <h4 className="react-course">React Course - Project 1</h4>
+        </nav>
+    )
+}
