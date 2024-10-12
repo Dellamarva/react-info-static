810474bb6424f9a3e32def56b8116f34114dc7d8
Greg Dellamura <g.dellamura@Gregs-MacBook-Pro-2.local>
2024-10-11 22:12:51 -0400

    added alt to navbar image

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,7 +3,7 @@ import React from "react";
 export default function Navbar() {
     return (
         <nav className="navbar">
-            <img className="react-logo" src="logo192.png" />
+            <img className="react-logo" src="logo192.png" alt="react-logo" />
             <h3 className="react-facts">ReactFacts</h3>
             <h4 className="react-course">React Course - Project 1</h4>
         </nav>
